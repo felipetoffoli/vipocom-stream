1ca52f8e404536b7e1d03555bfd2c2dc46c55974
Felipe Matheus Toffoli Martins <fmartins@psgtecnologiaaplicada.com.br>
2023-02-06 17:10:21 -0400

    configurações para stream e api

--- api/handler/stream.py
+++ api/handler/stream.py
@@ -7,18 +7,19 @@ class Stream:
     @staticmethod
     def check_on(key_stream):
         check = False
-        folder_files = os.listdir(r'/home/stream/hls')
+        folder_files = os.listdir(r'/home/stream')
 
         print(folder_files)
-        for file in folder_files:
-            if re.search(key_stream, file):
+        for filename in folder_files:
+            stremer_name = filename.replace('.m3u8', '')
+            if re.fullmatch(key_stream, stremer_name):
                 check = True
         return check
 
     @staticmethod
     def stremer_all():
         # folder_files = os.listdir(r'/home/stream/hls')
-        folder_files = glob.glob(r'/home/stream/hls/*.m3u8')
+        folder_files = glob.glob(r'/home/stream/*.m3u8')
         key_stremers = []
         if folder_files:
             for file in folder_files:
---
+++ docker-hls-stream/static/main.js
@@ -0,0 +1,13 @@
+if(Hls.isSupported()) {
+    let video = document.getElementById('video');
+    let hls = new Hls();
+    let stream = window.location.search.slice(1) || "stream";
+    hls.loadSource(`${stream}.m3u8`);
+    hls.attachMedia(video);
+    video.poster = 'offline.jpg';
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.addEventListener("click", () => video.play());
+        
+        video.play();
+    });
+}
